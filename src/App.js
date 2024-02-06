52d077f9b5af6820e4345d2ac8aed3586a471f67
kumsssss <kumaran.selvvaratnam@gmail.com>
2024-02-06 22:40:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = (noCount + 1) * 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Wouldn't you reconsider?😢",
+      "Is that your final answer?😢",
+      "You're breaking my heart😭",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  const getLoveBearGif = () => {
+    if (noCount >= 10) {
+      return "https://tenor.com/view/sad-cute-bear-crying-crying-bear-sweet-gif-15043229.gif";
+    } else if (noCount >= 5) {
+      return  "https://tenor.com/view/sad-cute-bear-please-gif-14322911.gif";
+    } else {
+      return "https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif";
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center h-screen -mt-16">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="Kissing bear" />
+          <div className="text-4xl font-bold my-4">Yay!!!Love you babe❤️❤️❤️</div>
+        </>
+      ) : (
+        <>
+            <img className="w-auto h-auto max-w-full max-h-full" src={getLoveBearGif()} alt="Love bear" />
+          <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
+          <div>
+            <button
+              className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
-
-export default App;
